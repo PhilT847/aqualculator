ee556842ef350ff6af86d80f6d8411841f20cfbf
Philip Thomson <pst16@georgetown.edu>
2023-12-18 14:59:26 -0500

    Organized calculator container sections

--- js/script.js
+++ js/script.js
@@ -1,1 +1,126 @@
 /* script.js */
+
+let operation = {
+    operand1: 0,
+    operand2: 0,
+    operator: ""
+};
+
+const calcContainer = document.querySelector("#calc-container");
+const calculator = document.createElement("div");
+const view = document.createElement("div");
+const topBtnContainer = document.createElement("div");
+const bottomBtnContainer = document.createElement("div");
+const lBottomBtnContainer = document.createElement("div");
+const rBottomBtnContainer = document.createElement("div");
+const numpad = document.createElement("div");
+
+createCalculator();
+
+function createCalculator() {
+
+    organizeCalculator();
+    createNumpad();
+}
+
+function organizeCalculator() {
+
+    calcContainer.appendChild(view);
+    calcContainer.appendChild(topBtnContainer);
+    calcContainer.appendChild(bottomBtnContainer);
+    bottomBtnContainer.appendChild(lBottomBtnContainer);
+    bottomBtnContainer.appendChild(rBottomBtnContainer);
+
+    view.setAttribute("id", "view");
+    topBtnContainer.setAttribute("id", "top-btn-container");
+    bottomBtnContainer.setAttribute("id", "bottom-btn-container");
+    lBottomBtnContainer.classList.add("left-bottom-btns");
+    rBottomBtnContainer.classList.add("right-bottom-btns");
+
+    view.textContent = "hey";
+    topBtnContainer.textContent = "hey";
+    rBottomBtnContainer.textContent = "hey";
+}
+
+function createNumpad() {
+
+    lBottomBtnContainer.appendChild(numpad);
+    numpad.setAttribute("id", "numpad-container");
+
+    for(let i = 9; i > -2; i--) {
+
+        const num = document.createElement("div");
+        num.classList.add("button");
+
+        if(i > -1) {
+
+            num.textContent = i;
+
+            num.addEventListener("click", () => {
+    
+                addNumber(i);
+            });
+
+            if(i == 0) { // 0 is wider
+                num.classList.add("zero-button");
+            }
+        }
+        else { // At -1, add decimal
+
+            num.textContent = ".";
+
+            num.addEventListener("click", () => {
+    
+                addDecimalPlace();
+            });
+        }
+
+        numpad.appendChild(num);
+    }
+}
+
+function createTopButtons() {
+
+
+}
+
+function createRightButtons() {
+
+    const multiplyButton = document.createElement("div");
+    const subtractButton = document.createElement("div");
+    const addButton = document.createElement("div");
+    const equalsButton = document.createElement("div");
+
+    multiplyButton.addEventListener("click", () => {
+
+        setOperator("*");
+    });
+    
+    subtractButton.addEventListener("click", () => {
+
+        setOperator("-");
+    });
+
+    addButton.addEventListener("click", () => {
+
+        setOperator("+");
+    });
+
+    equalsButton.addEventListener("click", () => {
+
+        setOperator("=");
+    });
+}
+
+function addNumber(num) {
+
+    operation.operand1 = num;
+}
+
+function addDecimalPlace() {
+
+}
+
+function setOperator(op) {
+
+}
